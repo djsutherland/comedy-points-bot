23cb748176adf707ee66e1bd21bb011becbd6df8
Danica J. Sutherland <djs@djsutherland.ml>
2026-03-19 22:02:45 -0700

    don't bother me with content-type warnings

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ from dotenv import load_dotenv
 from comedypoints import ComedyPointsBot
 
 
+def _allow_dm_log_record(record: logging.LogRecord) -> bool:
+    return not (
+        record.name == "reader"
+        and "NonXMLContentType('no Content-type specified')" in record.getMessage()
+    )
+
+
 def main():
     load_dotenv()
     discord.utils.setup_logging(level=os.environ.get("LOG_LEVEL", "INFO"))
@@ -23,6 +30,7 @@ def main():
             user_id=os.environ["DM_LOG_TARGET"],
             level=os.environ.get("DM_LOG_LEVEL", "WARNING"),
         )
+        handler.addFilter(_allow_dm_log_record)
         logger.addHandler(handler)
 
     bot = ComedyPointsBot()
