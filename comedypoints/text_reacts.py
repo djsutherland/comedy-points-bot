54368ac50f7c6d0cae951837ee07b47b576f0492
Danica J. Sutherland <djs@djsutherland.ml>
2026-04-15 16:17:25 -0700

    text_reacts: log a little more

--- comedypoints/text_reacts.py
+++ comedypoints/text_reacts.py
@@ -15,6 +15,7 @@ if os.environ.get("DEV_MODE"):
 else:
     STICKER_MAP = {"good": 1026120666182844536, "good.": 1026120666182844536}
 
+
 class TextReacts(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -26,10 +27,11 @@ class TextReacts(commands.Cog):
 
         # not going to bother with storing which have been reacted to because of this
         if datetime.datetime.now(datetime.UTC) - message.created_at > RECENCY_THRESH:
-            return # missed our chance
+            return  # missed our chance
 
         content = message.content.strip()
-        if (sticker_id := STICKER_MAP.get(content)):
+        if (sticker_id := STICKER_MAP.get(content)) is not None:
+            logger.info(f"{message.jump_url} said {content}")
             sticker = self.bot.get_sticker(sticker_id)
             if sticker is None:
                 sticker = await self.bot.fetch_sticker(sticker_id)
